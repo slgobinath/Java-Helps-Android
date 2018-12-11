a73822abff6ebfd4c1289db0bc6a39076f20220a
Gobinath <slgobinath@gmail.com>
2018-12-11 18:03:10 -0500

    Add Android Calculator in Kotlin

---
+++ projects/KotlinCalculator/app/src/main/java/com/javahelps/kotlincalculator/MainActivity.kt
@@ -0,0 +1,104 @@
+package com.javahelps.kotlincalculator
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import net.objecthunter.exp4j.ExpressionBuilder
+
+
+class MainActivity : AppCompatActivity() {
+
+    // TextView used to display the input and output
+    lateinit var txtInput: TextView
+
+    // Represent whether the lastly pressed key is numeric or not
+    var lastNumeric: Boolean = false
+
+    // Represent that current state is in error or not
+    var stateError: Boolean = false
+
+    // If true, do not allow to add another DOT
+    var lastDot: Boolean = false
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        txtInput = findViewById(R.id.txtInput)
+    }
+
+    /**
+     * Append the Button.text to the TextView
+     */
+    fun onDigit(view: View) {
+        if (stateError) {
+            // If current state is Error, replace the error message
+            txtInput.text = (view as Button).text
+            stateError = false
+        } else {
+            // If not, already there is a valid expression so append to it
+            txtInput.append((view as Button).text)
+        }
+        // Set the flag
+        lastNumeric = true
+    }
+
+    /**
+     * Append . to the TextView
+     */
+    fun onDecimalPoint(view: View) {
+        if (lastNumeric && !stateError && !lastDot) {
+            txtInput.append(".")
+            lastNumeric = false
+            lastDot = true
+        }
+    }
+
+    /**
+     * Append +,-,*,/ operators to the TextView
+     */
+    fun onOperator(view: View) {
+        if (lastNumeric && !stateError) {
+            txtInput.append((view as Button).text)
+            lastNumeric = false
+            lastDot = false    // Reset the DOT flag
+        }
+    }
+
+
+    /**
+     * Clear the TextView
+     */
+    fun onClear(view: View) {
+        this.txtInput.text = ""
+        lastNumeric = false
+        stateError = false
+        lastDot = false
+    }
+
+    /**
+     * Calculate the output using Exp4j
+     */
+    fun onEqual(view: View) {
+        // If the current state is error, nothing to do.
+        // If the last input is a number only, solution can be found.
+        if (lastNumeric && !stateError) {
+            // Read the expression
+            val txt = txtInput.text.toString()
+            // Create an Expression (A class from exp4j library)
+            val expression = ExpressionBuilder(txt).build()
+            try {
+                // Calculate the result and display
+                val result = expression.evaluate()
+                txtInput.text = result.toString()
+                lastDot = true // Result contains a dot
+            } catch (ex: ArithmeticException) {
+                // Display an error message
+                txtInput.text = "Error"
+                stateError = true
+                lastNumeric = false
+            }
+        }
+    }
+}
